98ef7d46dacffb7167715624ea4ac86189ed4c60
Tarundeep <tarundeepsaini003@gmail.com>
2024-04-10 18:55:08 +0530

    changes in about and email

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -13,11 +13,11 @@ const EmailSection = () => {
   return (
     <section id="contact" className="p-2 mt-16 ">
       <div>
-        <h5 className="text-3xl font-bold text-white my-2">Let's Contact</h5>
+        <h5 className="text-3xl font-bold text-white my-2">{`Let's Contact`}</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md ">
-          I'm currently looking for new opportunities, my inbox is always open.
+         {` I'm currently looking for new opportunities, my inbox is always open.
           Whether you have a question or just want to say hi, I'll try my best
-          to get back to you!
+          to get back to you!`}
         </p>
         <div className="socials flex flex-row gap-2">
           <Link
